a541e9ec026923e503652e3c0ac498bc90ad1f40
Kubi Kubisiak <mckubisiak@gmail.com>
2021-08-03 10:51:37 -0700

    added get a;; test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,6 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Fruit = require('../lib/models/fruit');
 
-
 describe('fruity routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -38,5 +37,31 @@ describe('fruity routes', () => {
       id: '1',
       ...fruit1,
     });
+  });
+
+  it('GET get all fruit', async () => {
+    const fruit1 = await Fruit.insert({
+      name: 'banana',
+      color: 'yellow',
+      ripe: false,
+    });
+
+    const fruit2 = await Fruit.insert({
+      name: 'apple',
+      color: 'red',
+      ripe: true,
+    });
+
+    const fruit3 = await Fruit.insert({
+      name: 'tangelo',
+      color: 'orange',
+      ripe: false,
+    });
+
+    return request(app)
+      .get('/api/v1/fruits/')
+      .then((res) => {
+        expect(res.body).toEqual([fruit1, fruit2, fruit3]);
+      });
   });
 });

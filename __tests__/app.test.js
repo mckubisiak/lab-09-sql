a6e20d6c3ba72775669d79c0987c4398c8624950
Kubi Kubisiak <mckubisiak@gmail.com>
2021-08-03 09:20:22 -0700

    post test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,25 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('lab-09-sql routes', () => {
+describe('fruity routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('POST creates a single fruit', async () => {
+
+    const fruit1 = {
+      name: 'banana',
+      color: 'yellow',
+      ripe: false
+    };
+    
+    const res = await request(app).post('/api/v1/fruits').send(fruit1);
+    
+    expect(res.body).toEqual({
+      id: '1',
+      ...fruit1, 
+    });
+    
   });
 });

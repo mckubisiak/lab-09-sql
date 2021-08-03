cfeae0d9824af08b65ba67e33a40429b82ccfebb
Kubi Kubisiak <mckubisiak@gmail.com>
2021-08-03 09:43:34 -0700

    wrote test for get route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,8 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Fruit = require('../models/fruit');
+
 
 describe('fruity routes', () => {
   beforeEach(() => {
@@ -9,19 +11,32 @@ describe('fruity routes', () => {
   });
 
   it('POST creates a single fruit', async () => {
-
     const fruit1 = {
       name: 'banana',
       color: 'yellow',
-      ripe: false
+      ripe: false,
     };
-    
+
     const res = await request(app).post('/api/v1/fruits').send(fruit1);
-    
+
     expect(res.body).toEqual({
       id: '1',
-      ...fruit1, 
+      ...fruit1,
     });
-    
+  });
+
+  it('GET get a single fruit', async () => {
+    const fruit1 = await Fruit.insert({
+      name: 'banana',
+      color: 'yellow',
+      ripe: false,
+    });
+
+    const res = await request(app).get(`/api/v1/fruits/${fruit1.id}`);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...fruit1,
+    });
   });
 });
